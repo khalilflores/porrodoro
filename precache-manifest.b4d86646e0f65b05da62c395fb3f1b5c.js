ea841b607270a6a4da6abe49a7b31f314c162256
khalilflores <khalilflores0@gmail.com>
2025-03-06 20:34:03 -0600

    Updates

---
+++ precache-manifest.b4d86646e0f65b05da62c395fb3f1b5c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "990f10a83f94fbcdf32bd6b2742c2246",
+    "url": "/porrodoro/index.html"
+  },
+  {
+    "revision": "2bf453bc33c5a1f156f9",
+    "url": "/porrodoro/static/js/2.b068ced6.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/porrodoro/static/js/2.b068ced6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e486fc5203755da2a74",
+    "url": "/porrodoro/static/js/main.ad3b2633.chunk.js"
+  },
+  {
+    "revision": "d1a06253b0c208838401",
+    "url": "/porrodoro/static/js/runtime-main.977c8ec8.js"
+  }
+]);
